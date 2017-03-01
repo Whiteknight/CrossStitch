e00b814be201c6d2408c158f53ac11e471ee5382
Andrew Whitworth <wknight8111@gmail.com>
2017-03-01 17:02:19 -0500

    Fixes

---
+++ Tests/StitchStart.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using CrossStitch.Stitch.V1;
+using CrossStitch.Stitch.V1.Stitch;
+using System;
+using System.Collections.Generic;
+
+namespace StitchStart.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Some kind of mechanism to attach Console.KeyAvailable to CancellationSource.Cancel.
+            StitchMessageManager manager = new StitchMessageManager(new MessageProcessor());
+            manager.StartRunLoop();
+        }
+    }
+
+    public class MessageProcessor : IToStitchMessageProcessor
+    {
+        public IEnumerable<FromStitchMessage> Process(ToStitchMessage message)
+        {
+            Console.WriteLine("Received {0} {1}: {2}", message.Id, message.ChannelName, message.Data);
+            return new[]
+            {
+                FromStitchMessage.Ack(message.Id)
+            };
+        }
+    }
+}
---
+++ Tests/StitchStart.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StitchStart.Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StitchStart.Client")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce85ab31-310a-4a64-b433-7fd1732914d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

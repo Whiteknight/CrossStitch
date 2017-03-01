71197def0de0ebce8da3068ac9dc8aede2d0fb74
Andrew Whitworth <wknight8111@gmail.com>
2017-02-28 20:03:36 -0500

    Rearrange some files

---
+++ Src/CrossStitch.Core/MessageBus/GenericResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CrossStitch.Core.Messages
+{
+    public class GenericResponse
+    {
+        public GenericResponse(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}

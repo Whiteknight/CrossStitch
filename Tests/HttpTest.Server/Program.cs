788b218789935efb29080d6bdfaefed5cd580778
Andrew Whitworth <wknight8111@gmail.com>
2017-03-20 15:54:48 -0400

    [Tests] Work on HttpTest test program
    Create a stitch for this test, so we can test status, start, stop and other commands.

---
+++ Tests/HttpTest.Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using CrossStitch.Core;
+using CrossStitch.Core.Modules.Data;
+using CrossStitch.Core.Modules.Data.Folders;
+using CrossStitch.Core.Modules.Logging;
+using CrossStitch.Core.Modules.Stitches;
+using CrossStitch.Http.NancyFx;
+using System;
+using System.Collections.Generic;
+using CrossStitch.Core.Models;
+using CrossStitch.Core.Modules.Data.InMemory;
+
+namespace HttpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodeConfig = NodeConfiguration.GetDefault();
+
+            using (var core = new CrossStitchCore(nodeConfig))
+            {
+                var httpConfiguration = HttpConfiguration.GetDefault();
+                var httpServer = new NancyHttpModule(httpConfiguration, core.MessageBus);
+                core.AddModule(httpServer);
+
+                var dataStorage = new InMemoryDataStorage();
+                var data = new DataModule(core.MessageBus, dataStorage);
+                core.AddModule(data);
+
+                var stitchesConfiguration = StitchesConfiguration.GetDefault();
+                var stitches = new StitchesModule(core, stitchesConfiguration);
+                core.AddModule(stitches);
+
+                var stitch = new StitchInstance
+                {
+                    Name = "HttpTest.Stitch",
+                    GroupName = new StitchGroupName("HttpTest", "Stitch", "1"),
+                    Adaptor = new InstanceAdaptorDetails
+                    {
+                        Type = AdaptorType.ProcessV1,
+                        Parameters = new Dictionary<string, string>
+                        {
+                            { CrossStitch.Stitch.ProcessV1.Parameters.DirectoryPath, "." },
+                            { CrossStitch.Stitch.ProcessV1.Parameters.ExecutableName, "HttpTest.Stitch.exe" }
+                        }
+                    },
+                    State = InstanceStateType.Running,
+                    LastHeartbeatReceived = 0
+                };
+                dataStorage.Save(stitch, true);
+
+                core.AddModule(new LoggingModule(core, Common.Logging.LogManager.GetLogger("CrossStitch")));
+
+                core.Start();
+                Console.ReadKey();
+                core.Stop();
+            }
+        }
+    }
+}
---
+++ Tests/HttpTest.Stitch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CrossStitch.Stitch.ProcessV1.Stitch;
+
+namespace HttpTest.Stitch
+{
+    class Program
+    {
+        private static StitchMessageManager _manager;
+
+        static void Main(string[] args)
+        {
+            _manager = new StitchMessageManager(args);
+            try
+            {
+                _manager.Start();
+                while (true)
+                {
+                    var msg = _manager.GetNextMessage();
+                    if (msg == null)
+                        continue;
+
+                    Log($"Message received {msg.ChannelName}, {msg.Id}");
+                    _manager.AckMessage(msg.Id);
+                }
+            }
+            catch (Exception e)
+            {
+                Log(e.ToString());
+            }
+            Log("Stopped");
+        }
+
+        public static void Log(string s)
+        {
+            _manager.SendLogs(new[] { s });
+            //File.AppendAllText("D:\\Test\\StitchStart.Client.txt", s + "\n");
+        }
+    }
+}

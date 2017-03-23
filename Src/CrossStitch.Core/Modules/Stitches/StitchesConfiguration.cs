ce2e4cf47d4edcb357630d0f8db8732cf16e6ad8
Andrew Whitworth <wknight8111@gmail.com>
2017-03-23 17:06:15 -0400

    [Stitches] Add some default values for StitchConfiguration

--- Src/CrossStitch.Core/Modules/Stitches/StitchesConfiguration.cs
+++ Src/CrossStitch.Core/Modules/Stitches/StitchesConfiguration.cs
@@ -15,6 +15,12 @@ namespace CrossStitch.Core.Modules.Stitches
 
         public void ValidateAndSetDefaults()
         {
+            if (string.IsNullOrEmpty(DataBasePath))
+                DataBasePath = ".\\StitchData";
+            if (string.IsNullOrEmpty(AppLibraryBasePath))
+                AppLibraryBasePath = ".\\StitchLibrary";
+            if (string.IsNullOrEmpty(RunningAppBasePath))
+                RunningAppBasePath = ".\\RunningStitches";
         }
     }
 }

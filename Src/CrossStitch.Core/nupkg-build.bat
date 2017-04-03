2bbdbc8117f0a2bd651dcd1485a4dc6bf00d08b0
Andrew Whitworth <wknight8111@gmail.com>
2017-04-03 08:35:27 -0400

    [Org] Fix several issues relating to nuget builds

--- Src/CrossStitch.Core/nupkg-build.bat
+++ Src/CrossStitch.Core/nupkg-build.bat
@@ -1,3 +1,3 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Core.csproj /t:Build /p:Configuration="Release"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Core.csproj /t:Build /p:Configuration="Release 4.5"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Core.csproj /t:Package
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Core.csproj /t:Build /p:Configuration="Release"
+::C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Core.csproj /t:Build /p:Configuration="Release 4.5"
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Core.csproj /t:Package /p:Configuration="Release"

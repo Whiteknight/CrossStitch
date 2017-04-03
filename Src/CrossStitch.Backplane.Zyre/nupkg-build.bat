0da200b083074c5965b0be1ac4946c2213adf6d2
Andrew Whitworth <wknight8111@gmail.com>
2017-04-03 13:10:58 -0400

    [Org] nuget script fixes for plugins

--- Src/CrossStitch.Backplane.Zyre/nupkg-build.bat
+++ Src/CrossStitch.Backplane.Zyre/nupkg-build.bat
@@ -1,3 +1,5 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Backplane.Zyre.csproj /t:Build /p:Configuration="Release"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Backplane.Zyre.csproj /t:Build /p:Configuration="Release 4.5"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Backplane.Zyre.csproj /t:Package
+set msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
+
+%msbuild% CrossStitch.Backplane.Zyre.csproj /t:Build /p:Configuration="Release"
+::C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Backplane.Zyre.csproj /t:Build /p:Configuration="Release 4.5"
+%msbuild% CrossStitch.Backplane.Zyre.csproj /t:Package /p:Configuration="Release"
--- Src/CrossStitch.Http.Nancy/nupkg-build.bat
+++ Src/CrossStitch.Http.Nancy/nupkg-build.bat
@@ -1,3 +1,5 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Http.NancyFx.csproj /t:Build /p:Configuration="Release"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Http.NancyFx.csproj /t:Build /p:Configuration="Release 4.5"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Http.NancyFx.csproj /t:Package
+set msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
+
+%msbuild% CrossStitch.Http.NancyFx.csproj /t:Build /p:Configuration="Release"
+::C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild CrossStitch.Http.NancyFx.csproj /t:Build /p:Configuration="Release 4.5"
+%msbuild% CrossStitch.Http.NancyFx.csproj /t:Package
